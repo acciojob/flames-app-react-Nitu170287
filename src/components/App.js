ee5f25ed356e9966ead172f5c662079334b9f7e1
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-18 23:02:25 -0400

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,74 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
+const App = () => {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [result, setResult] = useState("");
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+  function handleCalculateBtn() {
+    if (!firstName || !secondName) {
+      setResult("Please enter valid input");
+    } else {
+      let newFname = firstName;
+      let newSname = secondName;
+      for (let char of newFname) {
+        if (newSname.includes(char)) {
+          newSname = newSname.replace(char, "");
+          newFname = newFname.replace(char, "");
+        }
+      }
+      let strTotalLen = newFname.length + newSname.length;
+      switch (strTotalLen % 6) {
+        case 1:
+          setResult("Friends");
+          break;
+        case 2:
+          setResult("Love");
+          break;
+        case 3:
+          setResult("Affection");
+          break;
+        case 4:
+          setResult("Marriage");
+          break;
+        case 5:
+          setResult("Enemy");
+          break;
+        case 0:
+          setResult("Siblings");
+          break;
+      }
     }
-}
+  }
+  function hadleClearBtn() {
+    setFirstName("");
+    setSecondName("");
+    setResult("");
+  }
 
+  return (
+    <div id="main">
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        placeholder="Enter first name"
+        onChange={(e) => setFirstName(e.target.value.toLowerCase())}
+        value={firstName}
+      />
+      <input
+        type="text"
+        placeholder="Enter Second name"
+        onChange={(e) => setSecondName(e.target.value.toLowerCase())}
+        value={secondName}
+      />
+      <button onClick={handleCalculateBtn}>
+        Calculate Relationship Future
+      </button>
+      <button onClick={hadleClearBtn}>Clear</button>
+      <h3>{result}</h3>
+    </div>
+  );
+};
 
 export default App;
